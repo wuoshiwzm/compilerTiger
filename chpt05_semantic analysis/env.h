d3791d969207219b7b7669f6618e259c18c195d5
wuoshiwzm <wuoshiwzm@hotmail.com>
2023-12-29 09:11:25 +0800

    Create env.h

---
+++ chpt05_semantic analysis/env.h
@@ -0,0 +1,18 @@
+/**
+    环境
+*/
+typedef struct E_enventry_ *E_enventry;
+
+struct E_enventry_ {
+    enum {E_varEntry, E_funEntry} kind;
+    union {
+        struct {Ty_ty ty;} var;
+        struct {Ty_tyList formals; Ty_ty result;} fun;
+    } u;
+};
+
+E_enventry E_VarEntry(Ty_ty ty);
+E_enventry E_FunEntry(Ty_tyList formals, Ty_ty result);
+
+S_table E_base_tenv(void);  // 类型环境 Ty_ ty environment
+S_table E_base_venv(void);  // 值环境  E_ enventry environment
