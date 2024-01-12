e4ff27dc205a0d1a501ea9c391b689ccd3e6e5de
wuoshiwzm <wuoshiwzm@hotmail.com>
2024-01-12 22:16:59 +0800

    Update table.c

--- chpt05_semantic analysis/table.c
+++ chpt05_semantic analysis/table.c
@@ -46,10 +46,10 @@ static binder Binder(void *key, void *value, binder next, void *prevtop){
     b->value = value;
     b->next = next;
     b->prevtop = prevtop;
-    return b
+    return b;
 }
 
-// 空符号表 构造函数
+/* 空符号表 构造函数 */
 TAB_table TAB_empty(void){
     TAB_table t = checked_malloc(sizeof(*t));
     t->top = NULL;
@@ -65,18 +65,21 @@ TAB_table TAB_empty(void){
  *  将 key -> value 绑定添加进符号表 t，顶掉 key 之前的绑定
  */
 void TAB_enter(TAB_table t, void *key, void *value) {
-    int index
-    assert(t && key); // 非空判断
+    int index;
+    binder b;
+    assert(t && key); /* 非空判断 */
 
-    // 计算 key 的 hash
+    /* 计算 key 的 hash */
     index = ((unsigned) key) % TABSIZE;
 
-    // t->table[index] 为 table[index] 之前的绑定, 因为key的binder会替换前一个binder,这里将前一个 binder 保留下来.
-    // t->top 为 符号表的顶端, 保留不变
-    // t->table[index] 表示 b 的前一个 binder
-    // prevtop : 添加 binder 后这时 table 的 top
-    binder b = Binder(key,value,t->table[index],t->top);
-    t->table[index] = b; // 将新生成的绑定 替换原来的 table[index] 上的绑定
+    /* t->table[index] 为 table[index] 之前的绑定, 因为key的binder会替换前一个binder,这里将前一个 binder 保留下来.
+        t->top 为 符号表的顶端, 保留不变
+        t->table[index] 表示 b 的前一个 binder
+        prevtop : 添加 binder 后这时 table 的 top */
+
+    b = Binder(key,value,t->table[index],t->top);
+    t->table[index] = b; /* 将新生成的绑定 替换原来的 table[index] 上的绑定 */
+    t->top = key;
 }
 
 
@@ -93,7 +96,6 @@ void *TAB_look(TAB_table t, void *key) {
     index = ((unsigned)key) % TABSIZE;
 
     for (b = t->table[index]; b; b = b->next) {
-        b = t->table[i];
         if(b->key == key) return b->value;
     }
     return NULL;
@@ -114,14 +116,13 @@ void *TAB_pop(TAB_table t) {
     topkey = t->top;
     assert(topkey);
 
-    // 获取top 的 key 对应的 index
+    /* 获取top 的 key 对应的 index */
     int topkeyIndex = ((unsigned)topkey) % TABSIZE;
     b = t->table[topkeyIndex];
     assert(b);
 
-    // 将 b 的上一个 binder 替换为当前的 binder
+    /* 将 b 的上一个 binder 替换为当前的 binder */
     t->table[index] = b->next;
-
     return b->key;
 }
 
@@ -138,30 +139,30 @@ void  TAB_dump(TAB_table t, void(*show)(void *key, void *value)){
     int index = ((unsigned)topKey) % TABSIZE;
     binder b = t->table[index];
 
-    // top 的 binder 为NULL, 说明是空表
+    /* top 的 binder 为NULL, 说明是空表 */
     if(b == NULL) return;
 
-    // 将 topKey 对应的 binder 替换为上一个 binder ， 进行 遍历
+    /* 将 topKey 对应的 binder 替换为上一个 binder ， 进行 遍历 */
     t->table[index] = b->next;
     t->top = b->prevtop;
 
     show(b->key, b->value);
-    // 递归执行
+    /* 递归执行 */
     TAB_dump(t, show);
 
-    // 对 key　对应的 binder 都遍历完了 回复 table 的 top 的 table[index] 对应的 binder
+    /* 对 key　对应的 binder 都遍历完了 回复 table 的 top 的 table[index] 对应的 binder */
     t->top = topKey;
     t->table[index] = b;
 }
 
 
-// 展示所有的类型环境
-void TAB_show_tenv(TAB_table){
-    if(!t) {
-        printf("empty table");
-        return;
-    }
-}
+/* 展示所有的类型环境 */
+//void TAB_show_tenv(TAB_table){
+//    if(!t) {
+//        printf("empty table");
+//        return;
+//    }
+//}
 
 
 
