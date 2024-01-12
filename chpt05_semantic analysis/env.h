9abd3f69b8adf46b8623a6f5769e338c5dadffe1
wuoshiwzm <wuoshiwzm@hotmail.com>
2024-01-12 22:12:51 +0800

    Update env.h

--- chpt05_semantic analysis/env.h
+++ chpt05_semantic analysis/env.h
@@ -3,16 +3,17 @@
 */
 typedef struct E_enventry_ *E_enventry;
 
+/* 类型环境元素 */
 struct E_enventry_ {
     enum {E_varEntry, E_funEntry} kind;
     union {
-        struct {Ty_ty ty;} var;
-        struct {Ty_tyList formals; Ty_ty result;} fun;
+        struct {Ty_ty ty;} var; /* 变量类型 */
+        struct {Ty_tyList formals; Ty_ty result;} fun; /* 函数参数类型，返回值类型 */
     } u;
 };
 
 E_enventry E_VarEntry(Ty_ty ty);
 E_enventry E_FunEntry(Ty_tyList formals, Ty_ty result);
 
-S_table E_base_tenv(void);  // 类型环境 Ty_ ty environment
-S_table E_base_venv(void);  // 值环境  E_ enventry environment
+S_table E_base_tenv(void);  /* 类型环境 Ty_ ty environment */
+S_table E_base_venv(void);  /* 值环境  E_ enventry environment */
