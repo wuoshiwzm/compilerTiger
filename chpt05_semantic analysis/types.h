fddd8da8c4ef7f77259742d6144fdddc16d7ac1f
wuoshiwzm <wuoshiwzm@hotmail.com>
2024-01-12 22:18:29 +0800

    Update types.h

--- chpt05_semantic analysis/types.h
+++ chpt05_semantic analysis/types.h
@@ -10,7 +10,7 @@ typedef struct Ty_tyList_ *Ty_tyList;
 typedef struct Ty_field_ *Ty_field;
 typedef struct Ty_fieldList_ *Ty_fieldList;
 
-// 类型结构体
+/* 类型结构体 */
 struct Ty_ty_ {
     enum {
         // 基础类型
@@ -30,25 +30,26 @@ struct Ty_tyList_ {Ty_ty head; Ty_tyList tail;};
 struct Ty_field_ {S_symbol name; Ty_ty ty;};
 struct Ty_fieldList_ {Ty_field head; Ty_fieldList tail;};
 
-// 初始化基础类型 nil, int, string, double
+/* 初始化基础类型 nil, int, string, double */
 Ty_ty Ty_Nil(void);
 Ty_ty Ty_Int(void);
 Ty_ty Ty_String(void);
 Ty_ty Ty_Double(void);
 
-// void 类型
+/* void 类型 */
 Ty_ty Ty_Void(void);
 
-// 域列表类型
+/* 域列表类型 */
 Ty_ty Ty_Record(Ty_fieldList fields);
-// 数组类型
+/* 数组类型 */
 Ty_ty Ty_Array(Ty_ty ty);
-// 名称类型为名称（name）
+
+/* 自定义名称（name）的类型 */
 Ty_ty Ty_Name(S_symbol sym, Ty_ty ty);
 
-// 类型列表（链表）
+/* 类型列表（链表） */
 Ty_tyList Ty_TyList(Ty_ty head, Ty_tyList tail);
-// 域类型
+/* 域类型 */
 Ty_field Ty_Field(S_symbol name, Ty_ty ty);
 Ty_fieldList Ty_FieldList(Ty_field head, Ty_fieldList tail);
 
