6a5383a1c9843b5c8d4dcf79c67448a4a13f55e2
wuoshiwzm <wuoshiwzm@hotmail.com>
2024-01-19 17:16:18 +0800

    Update symbol.h

--- chpt05_semantic_analysis/symbol.h
+++ chpt05_semantic_analysis/symbol.h
@@ -1,25 +1,23 @@
 /**
     符号类
 */
-
 typedef struct S_symbol_ *S_symbol;
 
-// 获取 string 对应的 Symbol， 相同的字符串会获取到相同的 symbol
+/* 获取 string 对应的 Symbol， 相同的字符串会获取到相同的 symbol */
 S_symbol S_Symbol(string);
 
+string S_name(S_symbol); /* 符号 S_symbol 对应 名字 */
 
-string S_name(S_symbol); // 符号 S_symbol 对应 名字
-
-// 符号有
+/* 符号表 */
 typedef struct TAB_table_ * S_table;
 
-// 空符号表
+/* 空符号表 */
 S_table S_empty(void);
 
-// 将符号添加进符号表 S_table 中添加绑定: sym->value
+/* 将符号添加进符号表 S_table 中添加绑定: sym->value */
 void S_enter(S_table t, S_symbol sym, void *value);
 
-// 符号表 t 中查找符号 sym
+/* 符号表 t 中查找符号 sym */
 void *S_look(S_table t, S_symbol sym);
 
 void S_beginScope(S_table t);
