b4691299770147cf85ce2d430888b679fc8eb868
wuoshiwzm <wuoshiwzm@hotmail.com>
2024-01-19 17:16:06 +0800

    Update symbol.c

--- chpt05_semantic_analysis/symbol.c
+++ chpt05_semantic_analysis/symbol.c
@@ -3,6 +3,7 @@
 */
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include "util.h"
 #include "symbol.h"
 #include "table.h"
@@ -10,9 +11,11 @@
 struct S_symbol_ {string name; S_symbol next;};
 
 /* 创建符号 （符号名， 下一个符号） */
-static S_symbol mksymbol(string name, S_symbol next){
+static S_symbol mksymbol(string name, S_symbol next)
+{
     S_symbol s = checked_malloc(sizeof(*s));
-    s->name=checked_malloc(sizeof(strlen(name)) + 1);
+	
+	s->name=checked_malloc(sizeof(strlen(name)) + 1);
     strcpy(s->name, name);
     s->next = next;
     return s;
@@ -23,23 +26,35 @@ static S_symbol mksymbol(string name, S_symbol next){
 /* hashtable 为符号表，存储符号类 S_symbol */
 static S_symbol hashtable[SIZE];
 
-static unsigned int hash(char *s0) {
+static unsigned int hash(char *s0) 
+{
     unsigned int h=0;
     char *s;
     for(s=s0; *s; s++) {
         h = h * 65599 + *s;
     }
+	/* my-come-true
+    for(s=s0; *s; s++){
+    	if (isupper(*s) | *s == '_') h -= 'A'; else h -= 'a';
+        h = h + *s;
+    }
+	*/
     return h;
 }
+ 
+static int streq(string a, string b)
+{
+ return !strcmp(a,b);
+}
 
-/* 获取名字name 对应的符号 */
-S_symbol S_Symbol(string name) {
+/* S_symbol 构造函数，生成名字name 对应的符号 hashtable is a global-var save array of S_symbol*/
+S_symbol S_Symbol(string name) 
+{
     int index = hash(name) % SIZE;
     S_symbol syms = hashtable[index], sym;
 
-    for(sym = syms; sym; sym = sym->next){
-        if(0==strcmp(sym->name,name)) return sym;
-    }
+    for(sym = syms; sym; sym = sym->next)
+        if(streq(sym->name,name)) return sym;
 
     /* 没找到，新建符号 sym, 赋值给 hashtable[index] */
     sym = mksymbol(name,syms);
@@ -47,14 +62,27 @@ S_symbol S_Symbol(string name) {
     return sym;
 }
 
+/***********************/
+void S_distable()
+{
+	int i;
+	S_symbol t;
+	for (i = 0; i < SIZE; i++){
+		if (hashtable[i]) printf("%s\n", hashtable[i]->name);
+	}
+}
+/***********************/ 
 /* 获取符号的名字 */
-string S_name(S_symbol sym){
-    return sym->name;
+string S_name(S_symbol sym)
+{
+	return sym->name;
 }
 
 /* 获取空符号表 */
-S_table S_empty(void){ return TAB_empty();}
-
+S_table S_empty(void)
+{ 
+	return TAB_empty();
+}
 /* 将符号 sym 添加到符号表 t 中 */
 void S_enter(S_table t, S_symbol sym, void *value){
     TAB_enter(t,sym,value);
@@ -65,7 +93,6 @@ void *S_look(S_table t, S_symbol sym) {
     return TAB_look(t,sym);
 }
 
-
 /**
  * 初始化全局环境 （默认有一个全局符号表）
  */
@@ -74,15 +101,30 @@ void *S_look(S_table t, S_symbol sym) {
 static struct S_symbol_ marksym = {"<mark>",0};
 
 /* 进入环境的 符号表 */
-void S_beginScope(S_table t){
+void S_beginScope(S_table t)
+{
     /* 将基本元素添加到全局环境中， value为NULL: */
     /* S_enter(S_table t, S_symbol sym, void *value) , value */
     S_enter(t,&marksym, NULL);
 }
 
 /* 退出环境的符号表 */
-void S_endScope(S_table t){
+void S_endScope(S_table t)
+{
     S_symbol  s;
     /* 一直弹出符号，只到遇到基本符号 marksym 为止 */
-    do s= TAB_pop(t); while ( s!= &marksym );
+    do s= TAB_pop(t); 
+	while (s!= &marksym);
 }
+
+void S_dump(S_table t, void (*show)(S_symbol sym, void *binding)) {
+  TAB_dump(t, (void (*)(void *, void *)) show);
+}
+
+/*
+void S_show(S_table s, int type) {
+	if(type == 1) TAB_show_tenv(s);
+	else if (type == 2) TAB_show_venv(s);
+	else puts("unlonw table");
+}
+*/
