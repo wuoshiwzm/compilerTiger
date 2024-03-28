65de2af2f008b178ccb43434646dd30edbf0a268
wuoshiwzm <wuoshiwzm@hotmail.com>
2024-03-28 16:30:57 +0800

    Update env.h

--- chpt06_stack_frame/env.h
+++ chpt06_stack_frame/env.h
@@ -1,19 +1,23 @@
+#include "translate.h"
+#include "temp.h"
+#include "types.h"
 /**
     环境
 */
 typedef struct E_enventry_ *E_enventry;
 
-/* 类型环境元素 */
+// 带  level　的E_enventry, E_VarEntry， E_FunEntry  的类型环境元素
 struct E_enventry_ {
     enum {E_varEntry, E_funEntry} kind;
     union {
-        struct {Ty_ty ty;} var; /* 变量类型 */
-        struct {Ty_tyList formals; Ty_ty result;} fun; /* 函数参数类型，返回值类型 */
+        struct {Tr_access access; Ty_ty ty;} var; /* 变量 */
+        struct {Tr_level level; Temp_label label; Ty_tyList formals; Ty_ty result;} fun; /* 函数 */
     } u;
 };
 
-E_enventry E_VarEntry(Ty_ty ty);
-E_enventry E_FunEntry(Ty_tyList formals, Ty_ty result);
+E_enventry E_VarEntry(Tr_access access, Ty_ty ty);
+E_enventry E_FunEntry(Tr_level level, Temp_label label, Ty_tyList formals, Ty_ty result)
 
-S_table E_base_tenv(void);  /* 类型环境 Ty_ ty environment */
-S_table E_base_venv(void);  /* 值环境  E_ enventry environment */
+
+static S_table E_base_tenv(void);  /* 类型环境 Ty_ ty environment */
+static S_table E_base_venv(void);  /* 值环境  E_ enventry environment */
