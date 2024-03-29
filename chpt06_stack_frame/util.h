91f1df09dc9dc7f03f2d81119bb8463d8fb4df4a
wuoshiwzm <wuoshiwzm@hotmail.com>
2024-03-29 17:04:47 +0800

    Update util.h

--- chpt06_stack_frame/util.h
+++ chpt06_stack_frame/util.h
@@ -1,3 +1,6 @@
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
 #include <assert.h>
 
 typedef char * string;
@@ -12,3 +15,5 @@ string String(char *);
 typedef struct U_boolList_ * U_boolList;
 struct U_boolList_ {bool head; U_boolList tail;};
 U_boolList U_BoolList(bool head, U_boolList tail);
+
+#endif
