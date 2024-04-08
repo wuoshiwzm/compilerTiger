2932323e16fa01d0156e25e036585c5aa39c90ca
wuoshiwzm <wuoshiwzm@hotmail.com>
2024-04-08 17:11:54 +0800

    Update env.c

--- chpt06_stack_frame/env.c
+++ chpt06_stack_frame/env.c
@@ -8,8 +8,6 @@
 #include "translate.h"
 #include "env.h"
 
-
-
 /* 变量类型环境对象 可以添加到外层的 类型 环境中 */
 E_enventry E_VarEntry(Tr_access access, Ty_ty ty) {
     E_enventry venv;
@@ -23,7 +21,6 @@ E_enventry E_VarEntry(Tr_access access, Ty_ty ty) {
 /* 生成一个函数环境对象，可以添加到外层的 值 环境中 */
 E_enventry E_FunEntry(Tr_level level, Temp_label label, Ty_tyList formals, Ty_ty result) {
     E_enventry fenv = checked_malloc(sizeof(*fenv));
-
     fenv->kind = E_funEntry;
     fenv->u.fun.formals = formals;
     fenv->u.fun.result = result;
