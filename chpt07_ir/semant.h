eebc1465458e5a84a930d0d78116161b6c9e70c4
wuoshiwzm <wuoshiwzm@hotmail.com>
2024-06-19 15:35:00 +0800

    Update semant.h

--- chpt07_ir/semant.h
+++ chpt07_ir/semant.h
@@ -1,30 +1,52 @@
-#ifndef _SYMBOL_H_
-#define _SYMBOL_H_
+#ifndef _SEMANT_H_
+#define _SEMANT_H_
+
 /**
-    符号类
+    语义分析-类型检查
 */
-typedef struct S_symbol_ *S_symbol;
+F_fragList SEM_transProg(A_exp exp);
 
-/* 获取 string 对应的 Symbol， 相同的字符串会获取到相同的 symbol */
-S_symbol S_Symbol(string);
 
-string S_name(S_symbol); /* 符号 S_symbol 对应 名字 */
+static S_table E_base_tenv(void);  /* 类型环境 Ty_ ty environment */
+static S_table E_base_venv(void);  /* 值环境  E_ enventry environment */
 
-/* 符号表 */
-typedef struct TAB_table_ * S_table;
 
-/* 空符号表 */
-S_table S_empty(void);
+/* 将变量 转义成表达式类型 */
+static struct expty transVar(Tr_level level, S_table venv, S_table tenv, A_var v);
 
-/* 将符号添加进符号表 S_table 中添加绑定: sym->value */
-void S_enter(S_table t, S_symbol sym, void *value);
+/* 将表达式 转义成表达式类型 */
+static struct expty transExp(Tr_level level, S_table venv, S_table tenv, A_exp e);
 
-/* 符号表 t 中查找符号 sym */
-void *S_look(S_table t, S_symbol sym);
+Tr_exp transDec(Tr_level level, S_table venv, S_table tenv, A_dec d);
 
-void S_beginScope(S_table t);
-void S_endScope(S_table t);
+static Ty_ty transTy(S_table tb, A_ty ty);
 
-void S_show(S_table);
+static Ty_tyList makeFormalTyList(S_table t, A_fieldList fl);
+
+static Ty_ty actual_ty(Ty_ty ty);
+
+static bool args_match(Tr_level level, S_table v, S_table t, A_expList el, Ty_tyList fl, A_exp fun);
+
+static bool ty_match(Ty_ty a, Ty_ty b);
+
+static bool efields_match(Tr_level level, S_table v, S_table t, Ty_ty ty, A_exp e);
+
+static Ty_fieldList makeFieldTys(S_table t, A_fieldList fs);
+
+// 获取变量的访问
+static Tr_access varTrAccess();
+
+// 新的level 意味着发生 函数嵌套声明，则必然有 formals
+// ...Tr_level Tr_newLevel(Tr_level parent, Temp_label name, U_boolList formals);
+static Tr_level newLevel(Tr_level level, Ty_tyList formalTys, S_symbol funname);
+
+
+// 表达式类型
+struct expty {
+    Tr_exp exp;
+    Ty_ty ty;
+};
+
+struct expty expTy(Tr_exp exp, Ty_ty ty);
 
 #endif
